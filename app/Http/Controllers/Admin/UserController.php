9581df24799cf79131dc560a263a015638a51321
AidansCode <amurphey7@gmail.com>
2021-12-16 13:53:52 -0500

    Consistent spacing in controller

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,102 +3,86 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Hash;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Hash;
 
-class UserController extends Controller
-{
-   
-    public function index()
-    {
-        $users = User::all();
-		
-		return view('admin.user.index', [
-			'users' => $users
-		]);
-    }
-	
-	//Create user functions
-	
-	public function create() {
-		return view('admin.user.create');
-	}
-	
-	public function store(Request $request){
-		$request->validate([
-			'email' => 'required|string|min:1',
-			'name' => 'required|string|min:1',
-			'password' => 'required|confirmed|string|min:6'
-		]);
-		
-		$user = User::create([
-			'name' => $request->input('name'),
-			'email' => $request->input('email'),
-			'password' => Hash::make($request->input('password')),
-			'force_password_reset' => 1
-		]);
-		
-		return redirect(route('user.edit', ['user' => $user]))->with('success', 'Successfully created new user');
-	}
-	
-	// Edit user functions
-	
-    public function edit(User $user)
-    {
-        return view('admin.user.edit', [
-			'user' => $user
-		]);
-    }
-	
-	public function update(Request $request, User $user){
-		$request->validate([
-			'email' => 'required|string|min:1',
-			'name' => 'required|string|min:1',
-			'password' => 'required|confirmed|string|min:6',
-		]);
-		
-		$user->email = $request->input('email');
-		$user->name = $request->input('name');
-		$user->password = Hash::make($request->input('password'));
-		$user->save();
-		
-		return redirect(route('user.edit', ['user' => $user]))->with('success', 'Successfully updated user');
-		
-	}
-	
-	// Delete user functions
-	
-	public function delete(User $user) {
+class UserController extends Controller {
+
+  public function index() {
+    $users = User::all();
+
+    return view('admin.user.index', [
+    'users' => $users
+    ]);
+  }
+
+  public function create() {
+    return view('admin.user.create');
+  }
+
+  public function store(Request $request) {
+    $request->validate([
+      'email' => 'required|string|min:1',
+      'name' => 'required|string|min:1',
+      'password' => 'required|confirmed|string|min:6'
+    ]);
+
+    $user = User::create([
+      'name' => $request->input('name'),
+      'email' => $request->input('email'),
+      'password' => Hash::make($request->input('password')),
+      'force_password_reset' => 1
+    ]);
+
+    return redirect(route('user.edit', ['user' => $user]))->with('success', 'Successfully created new user');
+  }
+
+  public function edit(User $user) {
+    return view('admin.user.edit', [
+    'user' => $user
+    ]);
+  }
+
+  public function update(Request $request, User $user) {
+    $request->validate([
+      'email' => 'required|string|min:1',
+      'name' => 'required|string|min:1',
+      'password' => 'required|confirmed|string|min:6',
+    ]);
+
+    $user->email = $request->input('email');
+    $user->name = $request->input('name');
+    $user->password = Hash::make($request->input('password'));
+    $user->save();
+
+    return redirect(route('user.edit', ['user' => $user]))->with('success', 'Successfully updated user');
+  }
+
+  public function delete(User $user) {
     $user->delete();
 
     return redirect(route('user.index'))->with('success', 'Successfully deleted user!');
-	}
-	
-	//Change password functions
-	
-	public function change_password(User $user)
-    {
-        return view('admin.user.change_password', [
-			'user' => $user
-		]);
-    }
-	
-	public function update_password(Request $request, User $user){
-		$request->validate([
-			'password' => 'required|confirmed|string|min:6',
-		]);
-		
-		$user->password = Hash::make($request->input('password'));
-		$user->force_password_reset = 0;
-		$user->save();
-		
-		return redirect(route('location.index', ['user' => $user]))->with('success', 'Successfully updated password');
-		
-	}
-	
-	
-	
+  }
+
+  public function change_password(User $user) {
+    return view('admin.user.change_password', [
+    'user' => $user
+    ]);
+  }
+
+  public function update_password(Request $request, User $user) {
+    $request->validate([
+      'password' => 'required|confirmed|string|min:6',
+    ]);
+
+    $user->password = Hash::make($request->input('password'));
+    $user->force_password_reset = 0;
+    $user->save();
+
+    return redirect(route('location.index', ['user' => $user]))->with('success', 'Successfully updated password');
+  }
+
 
 }
